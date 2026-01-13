790655d1663f44f293483711e2b1ec9b4b3b5d70
Geunhyeok LEE <dev.ghlee@gmail.com>
2026-01-14 04:19:41 +0900

    feat: migrate common hmr runtime to es5 syntax (#2)
    Class syntax is not supported in the Hermes runtime.
    Therefore, code transformed to ES5 syntax is used to ensure backward compatibility.

---
+++ crates/rolldown_plugin_hmr/src/runtime/runtime-extra-dev-common-origin.js
@@ -0,0 +1,152 @@
+// @ts-check
+import {
+  __exportAll,
+  __reExport,
+  __toCommonJS,
+  __toESM,
+  // @ts-expect-error
+} from 'rolldown:runtime';
+
+class Module {
+  /**
+   * @type {{ exports: any }}
+   */
+  exportsHolder = { exports: null };
+  /**
+   * @type {string}
+   */
+  id;
+
+  /**
+   * @param {string} id
+   */
+  constructor(id) {
+    this.id = id;
+  }
+
+  get exports() {
+    return this.exportsHolder.exports;
+  }
+}
+
+/**
+ * @typedef {{ type: 'hmr:module-registered', modules: string[] }} DevRuntimeMessage
+ * @typedef {{ send(message: DevRuntimeMessage): void }} Messenger
+ */
+
+export class DevRuntime {
+  /**
+   * @param {Messenger} messenger
+   */
+  constructor(messenger) {
+    this.messenger = messenger;
+  }
+
+  /**
+   * @type {Record<string, Module>}
+   */
+  modules = {};
+  /**
+   * @param {string} _moduleId
+   */
+  createModuleHotContext(_moduleId) {
+    throw new Error('createModuleHotContext should be implemented');
+  }
+  /**
+   * @param {[string, string][]} _boundaries
+   */
+  applyUpdates(_boundaries) {
+    throw new Error('applyUpdates should be implemented');
+  }
+  /**
+   * @param {string} id
+   * @param {{ exports: any }} exportsHolder
+   */
+  registerModule(id, exportsHolder) {
+    const module = new Module(id);
+    module.exportsHolder = exportsHolder;
+    this.modules[id] = module;
+    this.sendModuleRegisteredMessage(id);
+  }
+  /**
+   * @param {string} id
+   */
+  loadExports(id) {
+    const module = this.modules[id];
+    if (module) {
+      return module.exportsHolder.exports;
+    } else {
+      console.warn(`Module ${id} not found`);
+      return {};
+    }
+  }
+
+  /**
+   * __esmMin
+   *
+   * @type {<T>(fn: any, res: T) => () => T}
+   * @internal
+   */
+  createEsmInitializer = (fn, res) => () => (fn && (res = fn((fn = 0))), res);
+  /**
+   * __commonJSMin
+   *
+   * @type {<T extends { exports: any }>(cb: any, mod: { exports: any }) => () => T}
+   * @internal
+   */
+  createCjsInitializer = (cb, mod) => () => (
+    mod || cb((mod = { exports: {} }).exports, mod), mod.exports
+  );
+  /** @internal */
+  __toESM = __toESM;
+  /** @internal */
+  __toCommonJS = __toCommonJS;
+  /** @internal */
+  __exportAll = __exportAll;
+  /**
+   * @param {boolean} [isNodeMode]
+   * @returns {(mod: any) => any}
+   * @internal
+   */
+  __toDynamicImportESM = (isNodeMode) => (mod) => __toESM(mod.default, isNodeMode);
+  /** @internal */
+  __reExport = __reExport;
+
+  sendModuleRegisteredMessage = (() => {
+    const cache = /** @type {string[]} */ ([]);
+    let timeout = /** @type {NodeJS.Timeout | null} */ (null);
+    let timeoutSetLength = 0;
+    const self = this;
+
+    /**
+     * @param {string} module
+     */
+    return function sendModuleRegisteredMessage(module) {
+      if (!self.messenger) {
+        return;
+      }
+      cache.push(module);
+      if (!timeout) {
+        timeout = setTimeout(
+          /** @returns void */
+          function flushCache() {
+            if (cache.length > timeoutSetLength) {
+              timeout = setTimeout(flushCache);
+              timeoutSetLength = cache.length;
+              return;
+            }
+
+            self.messenger.send({
+              type: 'hmr:module-registered',
+              modules: cache,
+            });
+            cache.length = 0;
+            timeout = null;
+            timeoutSetLength = 0;
+          },
+        );
+        timeoutSetLength = cache.length;
+      }
+    };
+  })();
+}
--- crates/rolldown_plugin_hmr/src/runtime/runtime-extra-dev-common.js
+++ crates/rolldown_plugin_hmr/src/runtime/runtime-extra-dev-common.js
@@ -1,152 +1,135 @@
-// @ts-check
-import {
-  __exportAll,
-  __reExport,
-  __toCommonJS,
-  __toESM,
-  // @ts-expect-error
-} from 'rolldown:runtime';
-
-class Module {
-  /**
-   * @type {{ exports: any }}
-   */
-  exportsHolder = { exports: null };
-  /**
-   * @type {string}
-   */
-  id;
-
-  /**
-   * @param {string} id
-   */
-  constructor(id) {
-    this.id = id;
-  }
-
-  get exports() {
-    return this.exportsHolder.exports;
+function _class_call_check(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
   }
 }
-
-/**
- * @typedef {{ type: 'hmr:module-registered', modules: string[] }} DevRuntimeMessage
- * @typedef {{ send(message: DevRuntimeMessage): void }} Messenger
- */
-
-export class DevRuntime {
-  /**
-   * @param {Messenger} messenger
-   */
-  constructor(messenger) {
-    this.messenger = messenger;
+function _defineProperties(target, props) {
+  for(var i = 0; i < props.length; i++){
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
   }
-
-  /**
-   * @type {Record<string, Module>}
-   */
-  modules = {};
-  /**
-   * @param {string} _moduleId
-   */
-  createModuleHotContext(_moduleId) {
-    throw new Error('createModuleHotContext should be implemented');
-  }
-  /**
-   * @param {[string, string][]} _boundaries
-   */
-  applyUpdates(_boundaries) {
-    throw new Error('applyUpdates should be implemented');
-  }
-  /**
-   * @param {string} id
-   * @param {{ exports: any }} exportsHolder
-   */
-  registerModule(id, exportsHolder) {
-    const module = new Module(id);
-    module.exportsHolder = exportsHolder;
-    this.modules[id] = module;
-    this.sendModuleRegisteredMessage(id);
-  }
-  /**
-   * @param {string} id
-   */
-  loadExports(id) {
-    const module = this.modules[id];
-    if (module) {
-      return module.exportsHolder.exports;
-    } else {
-      console.warn(`Module ${id} not found`);
-      return {};
-    }
-  }
-
-  /**
-   * __esmMin
-   *
-   * @type {<T>(fn: any, res: T) => () => T}
-   * @internal
-   */
-  createEsmInitializer = (fn, res) => () => (fn && (res = fn((fn = 0))), res);
-  /**
-   * __commonJSMin
-   *
-   * @type {<T extends { exports: any }>(cb: any, mod: { exports: any }) => () => T}
-   * @internal
-   */
-  createCjsInitializer = (cb, mod) => () => (
-    mod || cb((mod = { exports: {} }).exports, mod), mod.exports
-  );
-  /** @internal */
-  __toESM = __toESM;
-  /** @internal */
-  __toCommonJS = __toCommonJS;
-  /** @internal */
-  __exportAll = __exportAll;
-  /**
-   * @param {boolean} [isNodeMode]
-   * @returns {(mod: any) => any}
-   * @internal
-   */
-  __toDynamicImportESM = (isNodeMode) => (mod) => __toESM(mod.default, isNodeMode);
-  /** @internal */
-  __reExport = __reExport;
-
-  sendModuleRegisteredMessage = (() => {
-    const cache = /** @type {string[]} */ ([]);
-    let timeout = /** @type {NodeJS.Timeout | null} */ (null);
-    let timeoutSetLength = 0;
-    const self = this;
-
-    /**
-     * @param {string} module
-     */
-    return function sendModuleRegisteredMessage(module) {
-      if (!self.messenger) {
-        return;
-      }
-      cache.push(module);
-      if (!timeout) {
-        timeout = setTimeout(
-          /** @returns void */
-          function flushCache() {
-            if (cache.length > timeoutSetLength) {
-              timeout = setTimeout(flushCache);
-              timeoutSetLength = cache.length;
-              return;
-            }
-
-            self.messenger.send({
-              type: 'hmr:module-registered',
-              modules: cache,
-            });
-            cache.length = 0;
-            timeout = null;
-            timeoutSetLength = 0;
-          },
-        );
-        timeoutSetLength = cache.length;
-      }
-    };
-  })();
 }
+function _create_class(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
+import { __exportAll, __reExport, __toCommonJS, __toESM } from 'rolldown:runtime';
+var Module = /*#__PURE__*/ function() {
+  "use strict";
+  function Module(id) {
+      _class_call_check(this, Module);
+      this.exportsHolder = {
+          exports: null
+      };
+      this.id = id;
+  }
+  _create_class(Module, [
+      {
+          key: "exports",
+          get: function get() {
+              return this.exportsHolder.exports;
+          }
+      }
+  ]);
+  return Module;
+}();
+/** @type {typeof import('./runtime-extra-dev-common-origin.js').DevRuntime} */
+export var DevRuntime = /*#__PURE__*/ function() {
+  "use strict";
+  function DevRuntime(messenger) {
+      var _this = this;
+      _class_call_check(this, DevRuntime);
+      this.modules = {};
+      this.createEsmInitializer = function(fn, res) {
+          return function() {
+              return fn && (res = fn(fn = 0)), res;
+          };
+      };
+      this.createCjsInitializer = function(cb, mod) {
+          return function() {
+              return mod || cb((mod = {
+                  exports: {}
+              }).exports, mod), mod.exports;
+          };
+      };
+      this.__toESM = __toESM;
+      this.__toCommonJS = __toCommonJS;
+      this.__exportAll = __exportAll;
+      this.__toDynamicImportESM = function(isNodeMode) {
+          return function(mod) {
+              return __toESM(mod.default, isNodeMode);
+          };
+      };
+      this.__reExport = __reExport;
+      this.sendModuleRegisteredMessage = function() {
+          var cache = [];
+          var timeout = null;
+          var timeoutSetLength = 0;
+          var self = _this;
+          return function sendModuleRegisteredMessage(module) {
+              if (!self.messenger) {
+                  return;
+              }
+              cache.push(module);
+              if (!timeout) {
+                  timeout = setTimeout(function flushCache() {
+                      if (cache.length > timeoutSetLength) {
+                          timeout = setTimeout(flushCache);
+                          timeoutSetLength = cache.length;
+                          return;
+                      }
+                      self.messenger.send({
+                          type: 'hmr:module-registered',
+                          modules: cache
+                      });
+                      cache.length = 0;
+                      timeout = null;
+                      timeoutSetLength = 0;
+                  });
+                  timeoutSetLength = cache.length;
+              }
+          };
+      }();
+      this.messenger = messenger;
+  }
+  _create_class(DevRuntime, [
+      {
+          key: "createModuleHotContext",
+          value: function createModuleHotContext(_moduleId) {
+              throw new Error('createModuleHotContext should be implemented');
+          }
+      },
+      {
+          key: "applyUpdates",
+          value: function applyUpdates(_boundaries) {
+              throw new Error('applyUpdates should be implemented');
+          }
+      },
+      {
+          key: "registerModule",
+          value: function registerModule(id, exportsHolder) {
+              var module = new Module(id);
+              module.exportsHolder = exportsHolder;
+              this.modules[id] = module;
+              this.sendModuleRegisteredMessage(id);
+          }
+      },
+      {
+          key: "loadExports",
+          value: function loadExports(id) {
+              var module = this.modules[id];
+              if (module) {
+                  return module.exportsHolder.exports;
+              } else {
+                  console.warn("Module ".concat(id, " not found"));
+                  return {};
+              }
+          }
+      }
+  ]);
+  return DevRuntime;
+}();
