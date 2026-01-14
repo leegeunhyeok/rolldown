212d0bffba38888a5d1043f5b39e94a79f3ce5f1
Geunhyeok LEE <dev.ghlee@gmail.com>
2026-01-15 03:18:41 +0900

    fix: safety flush with `setTimeout` before initialization (#4)
    * fix: flush modules after initialization
    
    * update

--- crates/rolldown_plugin_hmr/src/runtime/runtime-extra-dev-common.js
+++ crates/rolldown_plugin_hmr/src/runtime/runtime-extra-dev-common.js
@@ -17,12 +17,15 @@ function _create_class(Constructor, protoProps, staticProps) {
   if (staticProps) _defineProperties(Constructor, staticProps);
   return Constructor;
 }
+// @ts-check
 import { __exportAll, __reExport, __toCommonJS, __toESM } from 'rolldown:runtime';
 var Module = /*#__PURE__*/ function() {
   "use strict";
   function Module(id) {
       _class_call_check(this, Module);
-      this.exportsHolder = {
+      /**
+ * @type {{ exports: any }}
+ */ this.exportsHolder = {
           exports: null
       };
       this.id = id;
@@ -43,74 +46,83 @@ export var DevRuntime = /*#__PURE__*/ function() {
   function DevRuntime(messenger) {
       var _this = this;
       _class_call_check(this, DevRuntime);
-      this.modules = {};
-      this.createEsmInitializer = function(fn, res) {
+      /**
+ * @type {Record<string, Module>}
+ */ this.modules = {};
+      /**
+ * __esmMin
+ *
+ * @type {<T>(fn: any, res: T) => () => T}
+ * @internal
+ */ this.createEsmInitializer = function(fn, res) {
           return function() {
               return fn && (res = fn(fn = 0)), res;
           };
       };
-      this.createCjsInitializer = function(cb, mod) {
+      /**
+ * __commonJSMin
+ *
+ * @type {<T extends { exports: any }>(cb: any, mod: { exports: any }) => () => T}
+ * @internal
+ */ this.createCjsInitializer = function(cb, mod) {
           return function() {
               return mod || cb((mod = {
                   exports: {}
               }).exports, mod), mod.exports;
           };
       };
-      this.__toESM = __toESM;
-      this.__toCommonJS = __toCommonJS;
-      this.__exportAll = __exportAll;
-      this.__toDynamicImportESM = function(isNodeMode) {
+      /** @internal */ this.__toESM = __toESM;
+      /** @internal */ this.__toCommonJS = __toCommonJS;
+      /** @internal */ this.__exportAll = __exportAll;
+      /**
+ * @param {boolean} [isNodeMode]
+ * @returns {(mod: any) => any}
+ * @internal
+ */ this.__toDynamicImportESM = function(isNodeMode) {
           return function(mod) {
               return __toESM(mod.default, isNodeMode);
           };
       };
-      this.__reExport = __reExport;
-      this.sendModuleRegisteredMessage = function() {
-          var cache = [];
-          var timeout = null;
-          var timeoutSetLength = 0;
+      /** @internal */ this.__reExport = __reExport;
+      this.cache = /** @type {string[]} */ [];
+      this.timeout = /** @type {NodeJS.Timeout | null} */ null;
+      this.timeoutSetLength = 0;
+      /** @type {(module: string) => void} */ this.sendModuleRegisteredMessage = function() {
           var self = _this;
-          return function sendModuleRegisteredMessage(module) {
+          /**
+   * @param {string} module
+   */ return function sendModuleRegisteredMessage(module) {
               if (!self.messenger) {
                   return;
               }
-              cache.push(module);
-              if (!timeout) {
-                  timeout = setTimeout(function flushCache() {
-                      if (cache.length > timeoutSetLength) {
-                          timeout = setTimeout(flushCache);
-                          timeoutSetLength = cache.length;
-                          return;
-                      }
-                      self.messenger.send({
-                          type: 'hmr:module-registered',
-                          modules: cache
-                      });
-                      cache.length = 0;
-                      timeout = null;
-                      timeoutSetLength = 0;
-                  });
-                  timeoutSetLength = cache.length;
-              }
+              self.cache.push(module);
+              this.timeout = safetyInvokeWithSetTimeout(self.flush.bind(this));
           };
       }();
       this.messenger = messenger;
   }
   _create_class(DevRuntime, [
       {
-          key: "createModuleHotContext",
+          /**
+ * @param {string} _moduleId
+ */ key: "createModuleHotContext",
           value: function createModuleHotContext(_moduleId) {
               throw new Error('createModuleHotContext should be implemented');
           }
       },
       {
-          key: "applyUpdates",
+          /**
+ * @param {[string, string][]} _boundaries
+ */ key: "applyUpdates",
           value: function applyUpdates(_boundaries) {
               throw new Error('applyUpdates should be implemented');
           }
       },
       {
-          key: "registerModule",
+          /**
+ * @param {string} id
+ * @param {{ exports: any }} exportsHolder
+ */ key: "registerModule",
           value: function registerModule(id, exportsHolder) {
               var module = new Module(id);
               module.exportsHolder = exportsHolder;
@@ -119,7 +131,9 @@ export var DevRuntime = /*#__PURE__*/ function() {
           }
       },
       {
-          key: "loadExports",
+          /**
+ * @param {string} id
+ */ key: "loadExports",
           value: function loadExports(id) {
               var module = this.modules[id];
               if (module) {
@@ -129,7 +143,37 @@ export var DevRuntime = /*#__PURE__*/ function() {
                   return {};
               }
           }
+      },
+      {
+          key: "flush",
+          value: function flush() {
+              if (this.cache.length > this.timeoutSetLength) {
+                  this.timeoutSetLength = this.cache.length;
+                  this.timeout = safetyInvokeWithSetTimeout(this.flush.bind(this));
+                  return;
+              }
+              this.messenger.send({
+                  type: 'hmr:module-registered',
+                  modules: this.cache
+              });
+              this.cache.length = 0;
+              this.timeoutSetLength = 0;
+              this.timeout = null;
+          }
       }
   ]);
   return DevRuntime;
 }();
+/**
+* In lower React Native versions, `setTimeout` is cannot be used in `rolldown:hmr` initialization phase because `InitializeCore` of React Native is not evaluated yet.
+* 
+* `rolldown:hmr` -> `InitializeCore` -> Define polyfills (e.g, `setTimeout`)
+*
+* @param {() => void} fn 
+*/ function safetyInvokeWithSetTimeout(fn) {
+  if (typeof setTimeout === 'function') {
+      return setTimeout(fn);
+  }
+  fn();
+  return null;
+}
